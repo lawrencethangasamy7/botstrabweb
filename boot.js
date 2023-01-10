6fce809374dea5d74e3a0b190653cbfc8ed6dc13
lawrence2052001 <lawrencethangasamy7@gmail.com>
2023-01-10 13:10:54 +0530

    updatelaal

---
+++ boot.js
@@ -0,0 +1,20 @@
+$('.carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:6,
+            nav:true,
+            loop:false
+        }
+    }
+})
